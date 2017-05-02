1f73ddab2f1d26105be0b613e87d773f7b37fb7a
Patrick Finnerty <patrick.finnerty@insa-lyon.fr>
2017-05-02 11:47:01 +0200

    Created class that will perform the api calls

---
+++ src/main/java/io/jawg/osmcontributor/api/ReportAPI.java
@@ -0,0 +1,20 @@
+/**
+ * This class provides the methods to call our own API the report issues via the application
+ */
+public class ReportingAPI
+{
+    public static final String SERVER_ADDRESS = "http://jcerv.heptacle.fr/";
+    public static final String CREATE_REPORT_ADDRESS = "http://jcerv.heptacle.fr/api/createReport";
+    public static final String UPDATE_REPORT_ADDRESS = "http://jcerv.heptacle.fr/api/updateReport";
+    public static final String UPLOAD_IMAGE_ADDRESS = "http://jcerv.heptacle.fr/api/uploadImage/";
+
+    /**
+     * This method will call the server and create a new report.
+     * @return
+     */
+    public static void createNewReport(String title, String details, String imageFilePath)
+    {
+        //TODO implement
+        return 0;
+    }
+}
