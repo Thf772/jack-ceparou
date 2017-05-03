42aa40d9411eaecb31f8cafe98c44c2a6c2f1ca5
Nathan Shiraini <nshiraini@heptacle.fr>
2017-05-03 11:06:17 +0200

    Starting to make the directions calculation

--- src/jackCeparou/java/io.jawg.osmcontributor/utils/DirectionsCalculator.java
+++ src/jackCeparou/java/io.jawg.osmcontributor/utils/DirectionsCalculator.java
@@ -1,8 +1,17 @@
 package io.jawg.osmcontributor.utils;
 
+import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.services.commons.models.Position;
+import com.mapbox.services.directions.v5.DirectionsCriteria;
+import com.mapbox.services.directions.v5.MapboxDirections;
 
 import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+import io.jawg.osmcontributor.ui.events.map.DestinationSelectedEvent;
+import io.jawg.osmcontributor.ui.events.map.OriginSelectedEvent;
+import io.jawg.osmcontributor.ui.events.map.StartDirectionsCalculationEvent;
 
 /**
  * @brief Calculates the directions between two POIs
@@ -14,7 +23,11 @@ public class DirectionsCalculator {
     private Position origin;
     private Position destination;
     private EventBus events;
+    private boolean enabled;
 
+    ////////////////////////////////////////////////////////////////////
+    /////     Constructors, destructors and (un)initializators     /////
+    ////////////////////////////////////////////////////////////////////
 
     public DirectionsCalculator(EventBus events) {
         this.origin = null;
@@ -25,15 +38,82 @@ public class DirectionsCalculator {
 
     public void enable() {
         events.register(this);
+        this.enabled = true;
     }
 
     public void disable() {
         events.unregister(this);
+        this.enabled = false;
     }
 
     public void setBus(EventBus events) {
-        this.disable();
+        if (this.enabled) this.disable();
         this.events = events;
-        this.enable();
     }
+
+    ////////////////////////////////////////////////////////////////////
+    /////     Getters and setters                                  /////
+    ////////////////////////////////////////////////////////////////////
+
+    public Position getOrigin() {
+        return this.origin;
+    }
+
+    public Position getDestination() {
+        return this.destination;
+    }
+
+    public void setOrigin(Position pos){
+        this.origin = pos;
+    }
+
+    public void setDestination(Position pos) {
+        this.destination = pos;
+    }
+
+    public boolean isEnabled() {
+        return this.enabled;
+    }
+
+    public boolean isReady() {
+        return this.isEnabled() &&
+                this.origin != null &&
+                this.destination != null;
+    }
+
+    ////////////////////////////////////////////////////////////////////
+    /////     Events                                               /////
+    ////////////////////////////////////////////////////////////////////
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onOriginSelectedEvent(OriginSelectedEvent e) {
+        this.setOrigin(e.getPosition());
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onDestinationSelectedEvent(DestinationSelectedEvent e) {
+        this.setDestination(e.getPosition());
+    }
+
+    @Subscribe(threadMode = ThreadMode.ASYNC)
+    public void onStartDirectionsCalculationEvent(StartDirectionsCalculationEvent e) {
+
+    }
+
+    ////////////////////////////////////////////////////////////////////
+    /////     Calculations                                         /////
+    ////////////////////////////////////////////////////////////////////
+
+    public void calculateDirections() {
+        if (!this.isReady()) return; // TODO Error signalling
+        MapboxDirections.Builder builder = new MapboxDirections.Builder()
+                .setOrigin(this.getOrigin())
+                .setDestination(this.getDestination())
+                .setOverview(DirectionsCriteria.OVERVIEW_FULL)
+                .setProfile(DirectionsCriteria.PROFILE_WALKING)
+                .setAccessToken(Mapbox.getAccessToken());
+
+    }
+
+
 }
