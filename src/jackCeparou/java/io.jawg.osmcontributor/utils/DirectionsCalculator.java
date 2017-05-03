e5166f6fbd6b9fa31a5aea6f702f8f20f747ac6f
Nathan Shiraini <nshiraini@heptacle.fr>
2017-05-03 10:07:35 +0200

    Creating directions calculator stub

---
+++ src/jackCeparou/java/io.jawg.osmcontributor/utils/DirectionsCalculator.java
@@ -0,0 +1,41 @@
+package io.jawg.osmcontributor.utils;
+
+import com.mapbox.services.commons.models.Position;
+
+import org.greenrobot.eventbus.EventBus;
+
+/**
+ * @brief Calculates the directions between two POIs
+ *
+ * This class will call MapBox's services API in order to get a wheelchair-friendly path between two points of interest.
+ */
+
+public class DirectionsCalculator {
+    private Position origin;
+    private Position destination;
+    private boolean isOriginSet;
+    private EventBus events;
+
+
+    public DirectionsCalculator(EventBus events) {
+        this.origin = null;
+        this.destination = null;
+        this.isOriginSet = false;
+        this.events = events;
+        this.enable();
+    }
+
+    public void enable() {
+        events.register(this);
+    }
+
+    public void disable() {
+        events.unregister(this);
+    }
+
+    public void setBus(EventBus events) {
+        this.disable();
+        this.events = events;
+        this.enable();
+    }
+}
