06e8c8b78c928eeed5cc769a652aa7fd18899df3
xia0ben <benoit.renault@insa-lyon.fr>
2017-05-04 04:09:14 +0200

    Merge about correction and add contrib activity.

--- src/jackCeparou/java/io/jawg/osmcontributor/ui/activity/ContributeActivity.java
+++ src/jackCeparou/java/io/jawg/osmcontributor/ui/activity/ContributeActivity.java
@@ -1,15 +1,71 @@
 package io.jawg.osmcontributor.ui.activity;
 
+import android.content.Context;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
+import javax.inject.Inject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.jawg.osmcontributor.OsmTemplateApplication;
 import io.jawg.osmcontributor.R;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class ContributeActivity extends AppCompatActivity {
+    WebView webView;
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ((OsmTemplateApplication) getApplication()).init(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contribute);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        webView = (WebView)findViewById(R.id.webView1);
+
+        WebSettings webSetting = webView.getSettings();
+        webSetting.setBuiltInZoomControls(true);
+        webSetting.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/first.html");
+    }
+
+    private class WebViewClient extends android.webkit.WebViewClient
+    {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url)
+        {
+            return super.shouldOverrideUrlLoading(view, url);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
